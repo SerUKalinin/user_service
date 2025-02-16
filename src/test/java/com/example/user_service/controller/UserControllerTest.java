c12d22603cf1561435708e0b165bc7fd3045f2dd
SerUKalinin <serukalinin@gmail.com>
2025-02-17 00:44:39 +0300

    проверка CI/CD (работает)

--- src/test/java/com/example/user_service/controller/UserControllerTest.java
+++ src/test/java/com/example/user_service/controller/UserControllerTest.java
@@ -1,13 +1,12 @@
 package com.example.user_service.controller;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class UserControllerTest {
 
-    @Test
-    public void testAlwaysFails() {
-        // Этот тест всегда будет падать
-        fail("Этот тест специально написан, чтобы проверить CI/CD");
-    }
+@Test
+    public void test() {
+
+}
+
 }
